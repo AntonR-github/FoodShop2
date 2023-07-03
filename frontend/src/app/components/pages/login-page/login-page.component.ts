56b518602527943f8c5e223cb9dcff2e11ff5e11
Anton <99226421+AntonR-github@users.noreply.github.com>
2023-07-03 07:51:34 +0300

    remake

---
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  loginForm!:FormGroup;
+  isSubmitted = false;
+
+
+  constructor(private formBuilder: FormBuilder
+    ,
+     private activatedRoute:ActivatedRoute,
+     private router:Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) return;
+
+    alert(`Email: ${this.fc.email.value} Password: ${this.fc.password.value}`)
+  }
+
+}
