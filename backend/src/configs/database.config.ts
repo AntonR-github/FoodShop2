fbaf3b7f8971844a85a5d27af2898d65c77c0a93
Anton <99226421+AntonR-github@users.noreply.github.com>
2023-07-05 22:32:57 +0300

    try

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,15 +1,25 @@
 import { connect, ConnectOptions } from 'mongoose';
 
 export const connectDB = async () => {
-    connect(process.env.MONGO_URI!, {
+    connect(process.env.MONGO_URI as string, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    } as ConnectOptions).then(() => {
-        console.log('Connected to database');
-    }
-    ).catch((err) => {
-        console.log('Failed to connect to database');
-        console.log(err);
-    }
+        useCreateIndex: true,
+        useFindAndModify: false
+    } as ConnectOptions);
+    console.log("Connected to MongoDB"
     );
+
 }
+
+
+// connect(process.env.MONGO_URI!, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// } as ConnectOptions).then(() => {
+//     console.log('Connected to database');
+// }
+// ).catch((err) => {
+//     console.log('Failed to connect to database');
+//     console.log(err);
+// }
