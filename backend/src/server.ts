1ab6420d29ec31ad96e51f850c634916505e5b69
Anton <99226421+AntonR-github@users.noreply.github.com>
2023-07-05 20:36:37 +0300

    fixes to publish

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router';
@@ -12,15 +12,20 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    credentials: true,
-    origin: ['http://localhost:4200']
+    credentials:true,
+    origin:["http://localhost:4200"]
 }));
 
-app.use('/api/foods', foodRouter)
-app.use('/api/users', userRouter)
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port = 5000;
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('Server started at http://localhost:' + port);
-});
+    console.log("Website served on http://localhost:" + port);
+})
